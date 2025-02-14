aa87837d31cddf52ba5317a1b2dda9acdd5b8484
Dari Kimanius <dkimanius@gmail.com>
2025-02-14 02:55:10 +0000

    WIP

--- voxelium/base/grid.py
+++ voxelium/base/grid.py
@@ -398,15 +398,24 @@ def fast_gaussian_filter(grid, kernel_sigma=None, kernel=None):
         k = make_gaussian_kernel(kernel_sigma).to(grid.device)
     else:
         raise RuntimeError("Either provide sigma or kernel.")
-    if len(grid.shape) == 5:  # 3D
+    
+    shape = grid.shape
+    if len(shape) == 2:
+        grid = grid.view(1, *shape)
+    if len(shape) == 3:
+        grid = grid.view(1, 1, *shape)
+
+    if len(shape) == 5:  # 3D
         grid = torch.nn.functional.conv3d(grid, k[None, None, :, None, None], padding='same')
         grid = torch.nn.functional.conv3d(grid, k[None, None, None, :, None], padding='same')
         grid = torch.nn.functional.conv3d(grid, k[None, None, None, None, :], padding='same')
-    elif len(grid.shape) == 4:  # 2D
+    elif len(shape) == 2:  # 2D
         grid = torch.nn.functional.conv2d(grid, k[None, None, :, None], padding='same')
         grid = torch.nn.functional.conv2d(grid, k[None, None, None, :], padding='same')
     else:
         raise NotImplementedError("Only 2D and 3D grids are supported.")
+
+    grid = grid.view(shape)
 
     return grid
 
