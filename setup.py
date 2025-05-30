c9c75b10d8d92fb18dce7b9c8fb688af9fc863de
Kevin Zhao <kevin.zhao@czii.org>
2025-05-29 22:22:06 -0700

    updated tag version

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ requires = [
 
 setup(
     name='Voxelium',
-    version='0.0.1a3',
+    version='0.0.1a4',
     packages=find_packages(),
     install_requires=requires,
     ext_modules=ext_modules,
